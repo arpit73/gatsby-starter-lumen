428854ff967d8acbb622bb8857fb4c5fbbdd8569
Arpit Bharti <rpittrer@gmail.com>
2019-08-26 03:15:17 +0530

    icon

--- config.js
+++ config.js
@@ -1,46 +1,48 @@
 'use strict';
 
 module.exports = {
-  url: 'https://arpit73.netlify.com',
-  pathPrefix: '/',
-  title: 'Arpit Bharti',
-  subtitle: 'GSoC 2019 at Mozilla | Python | React | OSS Lover | Anime Nerd | Audiophile | 3D Artist',
-  copyright: '',
-  disqusShortname: '',
-  postsPerPage: 7,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
+    url: 'https://arpit73.netlify.com',
+    pathPrefix: '/',
+    title: 'Arpit Bharti',
+    subtitle:
+        'GSoC 2019 at Mozilla | Python | React | OSS Lover | Anime Nerd | Audiophile | 3D Artist',
+    copyright: '',
+    disqusShortname: '',
+    postsPerPage: 7,
+    googleAnalyticsId: 'UA-73379983-2',
+    useKatex: false,
+    menu: [
+        {
+            label: 'Articles',
+            path: '/'
+        },
+        {
+            label: 'About me',
+            path: '/pages/about'
+        },
+        {
+            label: 'Contact me',
+            path: '/pages/contacts'
+        }
+    ],
+    author: {
+        name: 'Arpit Bharti',
+        photo: '/photo.jpg',
+        bio:
+            'GSoC 2019 @mozilla | Python | React | OSS Lover | Anime Nerd | Audiophile | 3D Artist',
+        contacts: {
+            email: 'arpitbharti73@gmail.com',
+            telegram: 'arpit73',
+            twitter: 'arpitbharti73',
+            github: 'arpit73',
+            instagram: '_arpitb_',
+            linkedin: 'arpitbharti',
+            facebook: '',
+            rss: '',
+            vkontakte: '',
+            line: '',
+            gitlab: '',
+            weibo: ''
+        }
     }
-  ],
-  author: {
-    name: 'Arpit Bharti',
-    photo: '/photo.jpg',
-    bio: 'GSoC 2019 @mozilla | Python | React | OSS Lover | Anime Nerd | Audiophile 🎧 | 3D Artist',
- contacts: {
-      email: 'arpitbharti73@gmail.com',
-      telegram: 'arpit73',
-      twitter: 'arpitbharti73',
-      github: 'arpit73',
-      instagram: '_arpitb_',
-      linkedin: 'arpitbharti',
-      facebook: '',
-      rss: '',
-      vkontakte: '',
-      line: '',
-      gitlab: '',
-      weibo: ''
-    }
-  }
 };
